444c965b97d807d3e98ba1d4d87b061d86779ff4
sunhengke <sunhengke@sjtu.edu.cn>
2020-09-16 22:31:36 +0800

    initial commit

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bytes"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"image"
+	"image/png"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	port := os.Getenv("port")
+	if port == "" {
+		port = "8080"
+	}
+
+	digits := make([]image.Image, 10)
+	cacheImages(&digits)
+
+	e := echo.New()
+	e.Use(middleware.Recover())
+	e.Use(middleware.Logger())
+
+	e.GET("/:id", func(c echo.Context) error {
+		id := c.Param("id")
+
+		m, err := generateMd5(id)
+		if err != nil {
+			log.Println(err)
+			return c.NoContent(http.StatusBadRequest)
+		}
+
+		count := updateCounter(m)
+		if count == "" {
+			log.Println("Fetch visitor count error.")
+			return c.NoContent(http.StatusInternalServerError)
+		}
+
+		img := generateImage(digits, count)
+		buf := new(bytes.Buffer)
+		err = png.Encode(buf, img)
+		if err != nil {
+			log.Println(err)
+			return c.NoContent(http.StatusInternalServerError)
+		}
+
+		expireTime := time.Now().Add(-10 * time.Minute).String()
+		c.Response().Header().Add("Expires", expireTime)
+		c.Response().Header().Add("Cache-Control", "no-cache,max-age=0,no-store,s-maxage=0,proxy-revalidate")
+
+		return c.Blob(http.StatusOK, "image/png", buf.Bytes())
+	})
+
+	e.Logger.Fatal(e.Start(":" + port))
+}
---
+++ utils.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/json"
+	"fmt"
+	"image"
+	"image/draw"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+type Count struct {
+	Value int `json:"value"`
+}
+
+func cacheImages(digits *[]image.Image) {
+	for i := range *digits {
+		file, _ := os.Open(fmt.Sprintf("digits/%d.png", i))
+		defer file.Close()
+		(*digits)[i], _, _ = image.Decode(file)
+	}
+}
+
+func generateMd5(id string) (string, error) {
+	w := md5.New()
+	if _, err := io.WriteString(w, id); err != nil {
+		return "", err
+	}
+	res := fmt.Sprintf("%x", w.Sum(nil))
+	return res, nil
+}
+
+func updateCounter(key string) string {
+	req, _ := http.NewRequest("GET", "https://api.countapi.xyz/hit/visitor-badge/"+key, nil)
+	resp, _ := http.DefaultClient.Do(req)
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return ""
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return ""
+	}
+	var count Count
+	if err = json.Unmarshal(body, &count); err != nil {
+		log.Println(err)
+		return ""
+	}
+	return strconv.Itoa(count.Value)
+}
+
+func generateImage(digits []image.Image, count string) image.Image {
+	length := len(count)
+	img := image.NewNRGBA(image.Rect(0, 0, 200*length, 200))
+	for i, _ := range count {
+		fmt.Println(count)
+		index, _ := strconv.Atoi(count[i : i+1])
+		draw.Draw(img, image.Rect(i*200, 0, 200*length, 200), digits[index], digits[index].Bounds().Min, draw.Over)
+	}
+	return img
+}
